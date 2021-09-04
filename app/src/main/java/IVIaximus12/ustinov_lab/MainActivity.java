f40cf689e04b99b7d824e35990543a19c0178613
Maximus12 <ymaks.u@yandex.ru>
2021-09-04 20:29:58 +0300

    Added gif loading using the Glide library.

--- app/src/main/java/IVIaximus12/ustinov_lab/MainActivity.java
+++ app/src/main/java/IVIaximus12/ustinov_lab/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton btnLeft;
     private ImageButton btnRight;
+    private ImageView mainGif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +20,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initView();
+        getGif();
     }
 
     private void initView() {
         btnLeft = findViewById(R.id.imageButtonLeft);
-        btnLeft = findViewById(R.id.imageButtonRight);
+        btnRight = findViewById(R.id.imageButtonRight);
+        mainGif = findViewById(R.id.image_gif);
     }
 
-
+    private void getGif() {
+        String url = "https://cdn.fishki.net/upload/post/201406/05/1275160/giphy-9.gif";
+        Glide.with(this)
+                .asGif()
+                .load(url)
+                .placeholder(R.drawable.kitty_placeholder)
+                .error(R.drawable.kitty_placeholder_error)
+                .into(mainGif);
+    }
 }
