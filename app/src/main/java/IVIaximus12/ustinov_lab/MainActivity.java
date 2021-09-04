9da42c957fb73251bf428c5fcd8b4bc9d4ddb80c
Maximus12 <ymaks.u@yandex.ru>
2021-09-04 15:02:19 +0300

    The main elements of MainActivity have been created. Added icons for buttons.

--- app/src/main/java/IVIaximus12/ustinov_lab/MainActivity.java
+++ app/src/main/java/IVIaximus12/ustinov_lab/MainActivity.java
@@ -3,12 +3,25 @@ package IVIaximus12.ustinov_lab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton btnLeft;
+    private ImageButton btnRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
     }
+
+    private void initView() {
+        btnLeft = findViewById(R.id.imageButtonLeft);
+        btnLeft = findViewById(R.id.imageButtonRight);
+    }
+
+
 }
