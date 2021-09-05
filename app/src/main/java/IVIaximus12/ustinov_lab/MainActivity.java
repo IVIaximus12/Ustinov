d3c94dab2c3c5d23e96a07d8d9d9d30386706767
Maximus12 <ymaks.u@yandex.ru>
2021-09-05 22:39:14 +0300

    Added check for Internet availability.

--- app/src/main/java/IVIaximus12/ustinov_lab/MainActivity.java
+++ app/src/main/java/IVIaximus12/ustinov_lab/MainActivity.java
@@ -1,11 +1,13 @@
 package IVIaximus12.ustinov_lab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -14,7 +16,6 @@ import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -24,12 +25,21 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //private properties
+
     private ImageButton btnLeft;
     private ImageButton btnRight;
     private ImageView mainGif;
     private TextView textView;
     private ArrayList<PostModel> postModels = new ArrayList<>();
     private int currentNumberModel = -1;
+    private View.OnClickListener oclBtnRight;
+    private View.OnClickListener oclBtnLeft;
+    private View.OnClickListener oclRepBtn;
+    private LinearLayout noInternetLayout;
+    private ConstraintLayout mainLayout;
+    private TextView repeatView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +48,35 @@ public class MainActivity extends AppCompatActivity {
 
         initView();
         getRandomPost();
-        //getGif();
+
+        initOnClickListener();
+        setOnClickListener();
+    }
 
 
-        View.OnClickListener oclBtnRight = new View.OnClickListener() {
+    //private methods
+
+    private void initView() {
+        btnLeft = findViewById(R.id.imageButtonLeft);
+        btnLeft.setEnabled(false);
+        btnRight = findViewById(R.id.imageButtonRight);
+        mainGif = findViewById(R.id.image_gif);
+        textView = findViewById(R.id.post_description);
+
+        noInternetLayout = findViewById(R.id.noInternetLayout);
+        mainLayout = findViewById(R.id.main_layout);
+        repeatView = findViewById(R.id.repeatButton);
+    }
+
+    private void initOnClickListener() {
+        oclBtnRight = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if (currentNumberModel == 0) {
+                    btnLeft.setEnabled(true);
+                }
+
                 if (currentNumberModel == postModels.size() - 1) {
                     getRandomPost();
                 } else {
@@ -56,11 +89,12 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        btnRight.setOnClickListener(oclBtnRight);
-
-        View.OnClickListener oclBtnLeft = new View.OnClickListener() {
+        oclBtnLeft = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (currentNumberModel == 1) {
+                    btnLeft.setEnabled(false);
+                }
                 currentNumberModel--;
                 PostModel post = postModels.get(currentNumberModel);
                 textView.setText(post.description);
@@ -68,18 +102,28 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        btnLeft.setOnClickListener(oclBtnLeft);
+        oclRepBtn = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isOnline()) {
+                    hideMainViews(false);
+                    getRandomPost();
+                }
+            }
+        };
     }
 
-    private void initView() {
-        btnLeft = findViewById(R.id.imageButtonLeft);
-        btnRight = findViewById(R.id.imageButtonRight);
-        mainGif = findViewById(R.id.image_gif);
-        textView = findViewById(R.id.post_description);
+    private void setOnClickListener() {
+        btnRight.setOnClickListener(oclBtnRight);
+        btnLeft.setOnClickListener(oclBtnLeft);
+        repeatView.setOnClickListener(oclRepBtn);
+    }
+
+    private boolean isOnline() {
+        return MyConnectivityManager.isOnline(this);
     }
 
     private void getGif(String url) {
-        //String url = "https://cdn.fishki.net/upload/post/201406/05/1275160/giphy-9.gif";
         Glide.with(this)
                 .asGif()
                 .load(url)
@@ -89,7 +133,22 @@ public class MainActivity extends AppCompatActivity {
                 .into(mainGif);
     }
 
-    void getRandomPost() {
+    private void hideMainViews(boolean flag) {
+        if (flag) {
+            noInternetLayout.setVisibility(View.VISIBLE);
+            mainLayout.setVisibility(View.GONE);
+        } else {
+            noInternetLayout.setVisibility(View.GONE);
+            mainLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void getRandomPost() {
+        if (!isOnline()) {
+            hideMainViews(true);
+            return;
+        }
+
         final Request request = new Request.Builder().url("https://developerslife.ru/random?json=true")
                 .method("GET", null)
                 .build();
--- app/src/main/java/IVIaximus12/ustinov_lab/PostModel.java
+++ app/src/main/java/IVIaximus12/ustinov_lab/PostModel.java
@@ -2,6 +2,9 @@ package IVIaximus12.ustinov_lab;
 
 class PostModel {
 
+
+    // public properties
+
     public String description;
     public String gifURL;
 }
